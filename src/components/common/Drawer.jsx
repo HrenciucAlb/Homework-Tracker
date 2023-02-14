e9f116d5b17d88f07e5b4df8761ff835f3c1aaca
Hrenciuc Albert <albert_albert38@yahoo.com>
2023-02-14 13:51:16 +0200

    Added the collapse sidebar functionality

--- src/components/common/Drawer.jsx
+++ src/components/common/Drawer.jsx
@@ -1,6 +1,15 @@
 import { Drawer, Box, Typography, IconButton, Icon, Menu } from "@mui/material";
 import MenuIcon from "@mui/icons-material/Menu";
+import ArrowBackIosNewSharpIcon from "@mui/icons-material/ArrowBackIosNewSharp";
+import { styled } from "@mui/material/styles";
+
 import { useState } from "react";
+
+const DrawerHeader = styled("div")(() => ({
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "flex-end",
+}));
 
 export const MuiDrawer = () => {
   const [isDrawerOpen, setisDrawerOpen] = useState(false);
@@ -21,6 +30,11 @@ export const MuiDrawer = () => {
         onClose={() => setisDrawerOpen(false)}
       >
         <Box p={2} width="250px" textAllign="center" role="presentation">
+          <DrawerHeader>
+            <IconButton onClick={() => setisDrawerOpen(false)}>
+              <ArrowBackIosNewSharpIcon />
+            </IconButton>
+          </DrawerHeader>
           <Typography variant="h6" component="div">
             Side Panel
           </Typography>
